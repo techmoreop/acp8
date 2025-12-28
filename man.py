919bc5aae9112f69d44ec54653e1f305c7102935
techmoreop <techmoreop@gmail.com>
2025-12-28 19:25:24 +0530

    Add files via upload

---
+++ man.py
@@ -0,0 +1,15 @@
+a = int(input("Enter a number "))
+b = int(input("Enter another number "))
+c = int(input("Enter one more number "))
+print("before  swaping")
+print(a)
+print(b)
+print(c)
+temp = a
+a = b
+b = c
+c = temp
+print("After swaping")
+print(a)
+print(b)
+print(c)
